ac1fca16f285b1824aa04b46276c891dd0c14cee
BotMvTvFilm <pasidhukaveesha@gmail.com>
2024-11-23 09:59:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=iy5lTRzT#223smvyI4NGdNB-vFRP0lIEQRQFDeyesLav1nZqDTUs' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=urIm3SjS#_y4VCGDbda6-cGIvHCFymRCXiXud2S7CUU7iQQ0TyvU' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || ',' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
